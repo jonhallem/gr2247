3bf3d04bf694d0900133cb4e34dc2032830e2b29
Mikkel Emil Lange Friis <mefriis@stud.ntnu.no>
2022-09-16 13:53:47 +0200

    fjernet ubrukte importer #25

--- 2247/src/test/java/BikeRentalApp/UserTest.java
+++ 2247/src/test/java/BikeRentalApp/UserTest.java
@@ -2,20 +2,11 @@ package BikeRentalApp;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UserTest {
-    
-    //User user;
 
     @Test
     @DisplayName("Tester det å opprette en ny bruker")
