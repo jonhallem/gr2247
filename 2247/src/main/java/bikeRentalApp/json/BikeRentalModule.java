2e05256a2afa7973d2c791da7783acacfc5972a6
Mikkel Emil Lange Friis <mefriis@stud.ntnu.no>
2022-09-20 19:36:14 +0200

    Skrive en mer utfyllende main-metode
    For å gjøre testing lettere
    
    #34

--- 2247/src/main/java/bikeRentalApp/json/BikeRentalModule.java
+++ 2247/src/main/java/bikeRentalApp/json/BikeRentalModule.java
@@ -18,25 +18,38 @@ public class BikeRentalModule extends SimpleModule {
 
     super(NAME, Version.unknownVersion());
     addSerializer(User.class, new UserSerializer());
+    //addDeserializer(User.class, new UserDerializer());
     
     addSerializer(Bike.class, new BikeSerializer());
+    addDeserializer(Bike.class, new BikeDeserializer());
 
     addSerializer(Place.class, new PlaceSerializer());
+    addDeserializer(Place.class, new PlaceDeserializer());
     
   }
 
   public static void main(String[] args) {
     User user = new User("name", "pass12");
     Bike bike = new Bike("BIKEIDN1", "Landeveissykkel", "Blå");
+    Place place = new Place("Munkholmen", 10);
 
     user.setBike(bike);
     System.out.println(user);
+
+    Bike bike2 = new Bike("BIKEIDN2", "Tandemsykkel", "Rød");
+    Bike bike3 = new Bike("BIKEIDN3", "Fatbike", "Rosa");
+
+    place.addBike(bike2);
+    place.addBike(bike3);
 
     ObjectMapper mapper = new ObjectMapper();
     mapper.registerModule(new BikeRentalModule());
 
     try {
+        System.out.println("JSON for user: ");
         System.out.println(mapper.writeValueAsString(user));
+        System.out.println("JSON for place: ");
+        System.out.println(mapper.writeValueAsString(place));
     } catch (Exception e) {
         System.err.println("Error");
         e.printStackTrace();
