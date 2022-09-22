d6806bd30863245a8d402385d4cf04cbcc4c339e
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2022-09-22 20:19:35 +0200

    Oppdatere BikeRentalModule med nye serialiserere
    La til serialiserere for container-klassene, før å støtte persistens.
    
    #17

--- 2247/src/main/java/bikeRentalApp/json/BikeRentalModule.java
+++ 2247/src/main/java/bikeRentalApp/json/BikeRentalModule.java
@@ -1,22 +1,26 @@
 package bikeRentalApp.json;
 
-import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.core.util.VersionUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import bikeRentalApp.core.Bike;
 import bikeRentalApp.core.Place;
+import bikeRentalApp.core.PlaceContainer;
 import bikeRentalApp.core.User;
+import bikeRentalApp.core.UserContainer;
 
 
 @SuppressWarnings("serial")
 public class BikeRentalModule extends SimpleModule {
     
-    private static final String NAME = "BikeRentalModule";
+  private static final String NAME = "BikeRentalModule";
+  private static final VersionUtil VERSION_UTIL = new VersionUtil() {
+  };
   
   public BikeRentalModule() {
 
-    super(NAME, Version.unknownVersion());
+    super(NAME, VERSION_UTIL.version());
     addSerializer(User.class, new UserSerializer());
     addDeserializer(User.class, new UserDeserializer());
     
@@ -25,7 +29,13 @@ public class BikeRentalModule extends SimpleModule {
 
     addSerializer(Place.class, new PlaceSerializer());
     addDeserializer(Place.class, new PlaceDeserializer());
-    
+
+    addSerializer(PlaceContainer.class, new PlaceContainerSerializer());
+    addDeserializer(PlaceContainer.class, new PlaceContainerDeserializer());
+
+    addSerializer(UserContainer.class, new UserContainerSerializer());
+    addDeserializer(UserContainer.class, new UserContainerDeserializer());
+
   }
 
   public static void main(String[] args) {
