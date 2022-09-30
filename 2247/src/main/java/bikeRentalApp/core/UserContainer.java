ee6459ec60e4a7d4fa3412193d5707cfbeb90639
Mikkel Emil Lange Friis <mefriis@stud.ntnu.no>
2022-09-30 10:50:15 +0200

    Legge til JavaDoc i UserContainer
    Lagt til mer utfyllende beskrivelse til dokumentasjonen i UserContainer
    
    #46

--- 2247/src/main/java/bikeRentalApp/core/UserContainer.java
+++ 2247/src/main/java/bikeRentalApp/core/UserContainer.java
@@ -4,22 +4,43 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * The {@code UserContainer} is a iterable class, holding a series of {@code User} objects. 
+ */
+
 public class UserContainer implements Iterable<User> {
     
     private List<User> users = new ArrayList<>();
 
+    /**
+     * Constructs a {@code UserContainer} object with a list of {@code User} objects. 
+     * @param userList
+     */
     public UserContainer(List<User> userList) {
         this.users = userList;
     }
 
+    /**
+     * Returns a new list of the registered {@code User} objects.
+     * @return List
+     */
     public List<User> getUsers() {
         return new ArrayList<>(users);
     }
 
+    /**
+     * Adds a {@code User} object to the list of users, {@code users}. 
+     * @param user
+     */
     public void addUser(User user) {
         this.users.add(user);
     }
 
+    /**
+     * 
+     * @param userName
+     * @return User
+     */
     public User findUser(String userName) {
         return this.users.stream().filter(user -> user.getUsername().equals(userName)).findFirst().get();
     }
