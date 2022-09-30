f3b599f8d5b3176c0772eb936d351cb3ba30e2af
Mikkel Emil Lange Friis <mefriis@stud.ntnu.no>
2022-09-30 11:57:11 +0200

    Legge til JavaDoc i PlaceContainer
    JavaDoc for metoder i PlaceContainer
    
    #44

--- 2247/src/main/java/bikeRentalApp/core/PlaceContainer.java
+++ 2247/src/main/java/bikeRentalApp/core/PlaceContainer.java
@@ -4,14 +4,26 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * The {@code PlaceContainer} is a iterable class, holding a series of {@code Place} objects. 
+ */
+
 public class PlaceContainer implements Iterable<Place> {
     
     private List<Place> places = new ArrayList<>();
 
+    /**
+     * Constructs a {@code PlaceContainer} object with a list of {@code Place} objects. 
+     * @param placeList
+     */
     public PlaceContainer(List<Place> placeList) {
         this.places = placeList;
     }
 
+    /**
+     * Returns a new list of the registered {@code Place} objects.
+     * @return List
+     */
     public List<Place> getPlaces() {
         return new ArrayList<>(places);
     }
