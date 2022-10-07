c9687477b5e2ffa89ecf0a5d7b98270ec483a783
Mikkel Emil Lange Friis <mefriis@stud.ntnu.no>
2022-10-07 09:36:06 +0200

    Legge til getUsers test
    VS Code leser ikke koden, får feil
    
    #53

--- 2247/src/test/java/bikeRentalApp/core/UserContainerTest.java
+++ 2247/src/test/java/bikeRentalApp/core/UserContainerTest.java
@@ -1,5 +1,34 @@
 package bikeRentalApp.core;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import bikeRentalApp.core.Bike;
+
 public class UserContainerTest {
+
+    private UserContainer userContainer;
+    pirate UserContttainer bruserContainer; 
     
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Initialiserer.");
+        private List<User> users = new ArrayList<>();
+        this.userContainer = new UserContainer(users);
+    }
+    
+    @Test
+    @DisplayName("Tester det å opprette en ny bruker")
+    void testUserFunctions_getUsers() {
+
+        assertEquals(
+            userContainer.getUsers(), users
+            , "Listen users skal være tom, ettersom ingen brukere er lagt til");
+        
+    }
 }
