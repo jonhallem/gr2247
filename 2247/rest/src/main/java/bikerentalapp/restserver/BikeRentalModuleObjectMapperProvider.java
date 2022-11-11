3daa9a8e7ade676c2f4b6bdda73b525fb9c4b483
Mikkel Emil Lange Friis <mefriis@stud.ntnu.no>
2022-11-11 11:35:11 +0100

    refactor: Legge til ny fil for objectMapper av BikeRentalPersistence
    BikeRentalModuleObjectMapperProvider gir mapper som brukes is BikeRentalConfig
    
    #106

---
+++ 2247/rest/src/main/java/bikerentalapp/restserver/BikeRentalModuleObjectMapperProvider.java
@@ -0,0 +1,35 @@
+package bikerentalapp.restserver;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.ext.ContextResolver;
+import jakarta.ws.rs.ext.Provider;
+import bikerentalapp.json.BikeRentalPersistence;
+
+/**
+ * Provides the Jackson module used for JSON serialization.
+ */
+@Provider
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class BikeRentalModuleObjectMapperProvider implements ContextResolver<ObjectMapper> {
+
+    private final ObjectMapper objectMapper;
+
+    /**
+     * Constructs a {@code BikeRentalModuleObjectMapperProvider} with a
+     * objectMapper, containing a mapper for the {@code BikeRentalPersistence}
+     * class.
+     */
+    public BikeRentalModuleObjectMapperProvider() {
+        objectMapper = BikeRentalPersistence.getObjectMapper();
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> type) {
+        return objectMapper;
+    }
+
+}
