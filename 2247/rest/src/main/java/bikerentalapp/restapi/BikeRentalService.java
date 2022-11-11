d019846d6327bcf2c4c6221a189f2fb5fbcf8841
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2022-11-11 08:55:39 +0100

    feat: Implementere hovedmetoder i BikeRentalService
    Foreløpig implementasjon for modellendring basert på serverkall.
    
    #106

--- 2247/rest/src/main/java/bikerentalapp/restapi/BikeRentalService.java
+++ 2247/rest/src/main/java/bikerentalapp/restapi/BikeRentalService.java
@@ -1,8 +1,11 @@
 package bikerentalapp.restapi;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
@@ -15,11 +18,27 @@ import bikerentalapp.core.User;
 import bikerentalapp.json.BikeRentalPersistence;
 
 /**
- * The top-level rest service for PlaceContainer.
+ * The top-level rest service for BikeRental.
  */
 @Path(BikeRentalService.BIKERENTAL_SERVICE_PATH)
 @Produces(MediaType.APPLICATION_JSON)
 public class BikeRentalService {
+
+    // Trenger metoder for:
+
+    // Signup -> endrer modell og returnerer user objekt (PUT)
+    // Login -> returnerer user objekt (GET)
+    // setUserPassword -> endrer modell og returnerer user objekt (POST)
+    // rentBike -> endrer modell og returnerer user objekt (POST)
+    // deliverBike -> endrer modell og returnerer user objekt (POST)
+    // getPlaces -> returnerer placecontainer med sykler (GET)
+
+    // Trenger aldri å bruke @consume (sender kun strings til server)
+    // Må bruke @produces når et user objekt og en placecontainer skal sendes
+    // tilbake
+    // Må kanskje bruke @Consumes(MediaType.TEXT_PLAIN) for å konsumere string sendt
+    // over http? Mulig ikke nødvendig pga queryparam?
+    // @QueryParam nyttig for å ta inn strings fra http
 
     public static final String BIKERENTAL_SERVICE_PATH = "bikerental";
 
@@ -31,44 +50,92 @@ public class BikeRentalService {
     @Context
     private BikeRentalPersistence bikeRentalPersistence;
 
-    /**
-     * The root resource, i.e. /placecontainer
-     *
-     * @return the PlaceContainer
-     */
+    @PUT
+    @Path("/signup")
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User signUpAndReturnUser(@FormParam("username") String username, @FormParam("password") String password) {
+        LOG.debug("signup: " + username + ", " + password);
+        try {
+            return this.bikeRentalManager.signUp(username, password);
+        } catch (Exception e) {
+            // TODO
+            return null;
+        }
+    }
+
     @GET
-    public BikeRentalManager getBikeRentalManager() {
-        LOG.debug("getBikeRenatlManager: " + this.bikeRentalManager);
-        return this.bikeRentalManager;
+    @Path("/login")
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User logInAndReturnUser(@FormParam("username") String username, @FormParam("password") String password) {
+        LOG.debug("login: " + username + ", " + password);
+        try {
+            return this.bikeRentalManager.logIn(username, password);
+        } catch (Exception e) {
+            // TODO
+            return null;
+        }
     }
 
-    /**
-     * Returns the PlaceContainer object
-     * (as a resource to support chaining path elements).
-     * 
-     * @return the User
-     */
-    @Path("/placecontainer")
-    public PlaceContainerResource getPlaceContainer() {
-        LOG.debug("Sub-resource for PlaceContainer");
-        return new PlaceContainerResource(this.bikeRentalManager);
+    @POST
+    @Path("/setpassword")
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User changePasswordAndReturnUser(@FormParam("username") String username,
+            @FormParam("newpassword") String newpassword) {
+        LOG.debug("changePassword: " + username + ", " + newpassword);
+        try {
+            return this.bikeRentalManager.setUserPassword(username, newpassword);
+        } catch (Exception e) {
+            // TODO
+            return null;
+        }
     }
 
-    /**
-     * Returns the User with the provided name
-     * (as a resource to support chaining path elements).
-     * This supports all requests referring to Users by name.
-     *
-     * @param name the name of the todo list
-     * 
-     * @return the User
-     */
-    @Path("/user/{name}/{password}")
-    public UserResource getUser(@PathParam("name") String name, @PathParam("password") String password) {
-        User user = this.bikeRentalManager.getBikeRentalPersistence().readUserContainer().findUser(name);
-        LOG.debug("Sub-resource for User " + name);
-        UserResource userResource = new UserResource(this.bikeRentalManager, name, password);
-        return userResource;
+    @POST
+    @Path("/rentbike")
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User rentBikeAndReturnUser(@FormParam("placename") String placeName,
+            @FormParam("bikeid") String bikeId, @FormParam("username") String username) {
+        LOG.debug("rentBike: " + placeName + ", " + bikeId + ", " + username);
+        try {
+            return this.bikeRentalManager.rentBike(placeName, bikeId, username);
+        } catch (Exception e) {
+            // TODO
+            return null;
+        }
+    }
+
+    @POST
+    @Path("/deliverbike")
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User deliverBikeAndReturnUser(@FormParam("username") String username,
+            @FormParam("placename") String placeName) {
+        LOG.debug("deliverBike: " + username + ", " + placeName);
+        try {
+            return this.bikeRentalManager.deliverBike(username, placeName);
+        } catch (Exception e) {
+            // TODO
+            return null;
+        }
+    }
+
+    @GET
+    @Path("/getplaces")
+    @Produces(MediaType.APPLICATION_JSON)
+    public PlaceContainer getPlaces() {
+        LOG.debug("getPlaces");
+        try {
+            // Her må metoder byttes på slik at controller bruker placecontainer i stedet
+            // for en liste med places
+            return this.bikeRentalManager.getBikeRentalPersistence().readPlaceContainer();
+        } catch (Exception e) {
+            // TODO
+            return null;
+        }
     }
 
 }
