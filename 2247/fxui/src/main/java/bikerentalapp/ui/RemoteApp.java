74709369ab60aaa90138b5becdd28660782f99eb
Jon Pape Hallem <jonph@stud.ntnu.no>
2022-11-14 22:25:55 +0100

    style: Forbedre remoteApp checkstyle og javadoc
    Endre remoteApp med checkstyle og javadoc forbedringer
    
    #109

--- 2247/fxui/src/main/java/bikerentalapp/ui/RemoteApp.java
+++ 2247/fxui/src/main/java/bikerentalapp/ui/RemoteApp.java
@@ -1,19 +1,27 @@
 package bikerentalapp.ui;
 
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * A class that launches the Bike Rental app with remote model access.
+ */
 public class RemoteApp extends Application {
 
+    /**
+     * Launches the Bike Rental app with remote model access.
+     *
+     * @param stage the stage to launch.
+     */
     @Override
     public void start(Stage stage) throws IOException {
         RemoteApp app = (RemoteApp) this;
-        FXMLLoader fxmlLoader = new FXMLLoader(app.getClass().getResource("BikeRentalRemoteApp.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(app.getClass()
+                .getResource("BikeRentalRemoteApp.fxml"));
         Parent parent = fxmlLoader.load();
         stage.setScene(new Scene(parent));
         stage.show();
