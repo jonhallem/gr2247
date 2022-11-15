fd1b1f186a55ba3911d39c07cb5790afa242bae0
Jon Pape Hallem <jonph@stud.ntnu.no>
2022-11-15 21:35:23 +0100

    style: Forbedre checkstyle BikeDeserializer
    Utrede checkstyle konsistens i BikeDeserializer
    
    #85

--- 2247/core/src/main/java/bikerentalapp/json/internal/BikeDeserializer.java
+++ 2247/core/src/main/java/bikerentalapp/json/internal/BikeDeserializer.java
@@ -1,7 +1,6 @@
 package bikerentalapp.json.internal;
 
-import java.io.IOException;
-
+import bikerentalapp.core.Bike;
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.TreeNode;
@@ -10,13 +9,13 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
-
-import bikerentalapp.core.Bike;
+import java.io.IOException;
 
 public class BikeDeserializer extends JsonDeserializer<Bike> {
 
     @Override
-    public Bike deserialize(JsonParser parser, DeserializationContext context) throws IOException, JacksonException {
+    public Bike deserialize(JsonParser parser,
+            DeserializationContext context) throws IOException, JacksonException {
         TreeNode treeNode = parser.getCodec().readTree(parser);
         return this.deserialize((JsonNode) treeNode);
     }
@@ -24,11 +23,12 @@ public class BikeDeserializer extends JsonDeserializer<Bike> {
     public Bike deserialize(JsonNode jsonNode) {
         if (jsonNode instanceof ObjectNode) {
             ObjectNode objectNode = (ObjectNode) jsonNode;
-            JsonNode iDNode = objectNode.get("iD");
+            JsonNode idNode = objectNode.get("iD");
             JsonNode typeNode = objectNode.get("type");
             JsonNode colourNode = objectNode.get("colour");
-            if (iDNode instanceof TextNode && typeNode instanceof TextNode && colourNode instanceof TextNode) {
-                return new Bike(((TextNode) iDNode).asText(), ((TextNode) typeNode).asText(),
+            if (idNode instanceof TextNode && typeNode instanceof TextNode
+                    && colourNode instanceof TextNode) {
+                return new Bike(((TextNode) idNode).asText(), ((TextNode) typeNode).asText(),
                         ((TextNode) colourNode).asText());
             }
         }
