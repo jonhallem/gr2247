a47bf5fbcf0cbe0f25cd1478ed966db4cbdab172
Jon Pape Hallem <jonph@stud.ntnu.no>
2022-11-15 21:42:29 +0100

    style: Fikse checkstyle UserContainerDeserializer
    Endre checkstyle for UserContainerDeserializer
    
    #81

--- 2247/core/src/main/java/bikerentalapp/json/internal/UserContainerDeserializer.java
+++ 2247/core/src/main/java/bikerentalapp/json/internal/UserContainerDeserializer.java
@@ -1,9 +1,7 @@
 package bikerentalapp.json.internal;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
+import bikerentalapp.core.User;
+import bikerentalapp.core.UserContainer;
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.TreeNode;
@@ -12,9 +10,9 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-
-import bikerentalapp.core.User;
-import bikerentalapp.core.UserContainer;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserContainerDeserializer extends JsonDeserializer<UserContainer> {
 
