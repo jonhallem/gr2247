2a4ea558c483241aee3ffa02f822f923fb905855
Jon Pape Hallem <jonph@stud.ntnu.no>
2022-11-15 21:41:25 +0100

    style: Endre checkstyle i PlaceSerializer
    Utrede checkstyle konsistens i PlaceSerializer
    
    #80

--- 2247/core/src/main/java/bikerentalapp/json/internal/PlaceSerializer.java
+++ 2247/core/src/main/java/bikerentalapp/json/internal/PlaceSerializer.java
@@ -1,13 +1,11 @@
 package bikerentalapp.json.internal;
 
-import java.io.IOException;
-
+import bikerentalapp.core.Bike;
+import bikerentalapp.core.Place;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-
-import bikerentalapp.core.Bike;
-import bikerentalapp.core.Place;
+import java.io.IOException;
 
 public class PlaceSerializer extends JsonSerializer<Place> {
 
@@ -21,15 +19,17 @@ public class PlaceSerializer extends JsonSerializer<Place> {
      */
 
     @Override
-    public void serialize(Place place, JsonGenerator jGen, SerializerProvider serializerProvider) throws IOException {
-        jGen.writeStartObject();
-        jGen.writeStringField("name", place.getName());
-        jGen.writeStringField("maximumNumberOfBikes", String.valueOf(place.getMaximumNumberOfBikes()));
-        jGen.writeArrayFieldStart("bikes");
+    public void serialize(Place place, JsonGenerator jsonGen,
+            SerializerProvider serializerProvider) throws IOException {
+        jsonGen.writeStartObject();
+        jsonGen.writeStringField("name", place.getName());
+        jsonGen.writeStringField("maximumNumberOfBikes",
+                String.valueOf(place.getMaximumNumberOfBikes()));
+        jsonGen.writeArrayFieldStart("bikes");
         for (Bike bike : place) {
-            jGen.writeObject(bike);
+            jsonGen.writeObject(bike);
         }
-        jGen.writeEndArray();
-        jGen.writeEndObject();
+        jsonGen.writeEndArray();
+        jsonGen.writeEndObject();
     }
 }
