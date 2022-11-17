53af71820bcbe4dc65bdbbfa7c2947617bdbdd27
Mikkel Emil Lange Friis <mefriis@stud.ntnu.no>
2022-11-17 19:15:48 +0100

    build: :bricks: Legge til krav i modul-info til rest modul
    Krav for core, jersey, glassfish og slf4j
    
    #87

--- 2247/rest/src/main/java/module-info.java
+++ 2247/rest/src/main/java/module-info.java
@@ -1,3 +1,14 @@
 module bikerentalapp.rest {
+    requires jakarta.ws.rs;
 
+    requires jersey.common;
+    requires jersey.server;
+    requires jersey.media.json.jackson;
+
+    requires org.glassfish.hk2.api;
+    requires org.slf4j;
+
+    requires bikerentalapp.core;
+
+    opens bikerentalapp.restapi to jersey.server;
 }
